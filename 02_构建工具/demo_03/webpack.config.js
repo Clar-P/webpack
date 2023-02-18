aabef87e2917af1c2003e44242a3c27243c62112
pujing <316910570@qq.com>
2023-02-18 20:12:02 +0800

    提交

--- 02_构建工具/demo_03/webpack.config.js
+++ 02_构建工具/demo_03/webpack.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mode:"production" // 设置打包的模式：production 表示生产模式  development 开发模式
+    mode:"production", // 设置打包的模式：production 表示生产模式  development 开发模式
+     entry:"./hello/hello.js", // 用来指定打包时的主文件 默认 ./src/index.js
 }
